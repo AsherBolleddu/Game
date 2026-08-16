cb11496a6a346e7de6501f7d1416628a8afd7e98
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2026-08-16 09:58:57 -0500

    Set up SDL3

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+int main(int, char**)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* Window(SDL_CreateWindow("Hello Window", 800, 300, 0));
+    SDL_GetWindowSurface(Window);
+    SDL_UpdateWindowSurface(Window);
+
+    bool isRunning { true };
+    SDL_Event Event;
+    while (isRunning)
+        while (SDL_PollEvent(&Event))
+            if (Event.type == SDL_EVENT_QUIT)
+                isRunning = false;
+
+    SDL_DestroyWindow(Window);
+    SDL_Quit();
+
+    return 0;
+}
